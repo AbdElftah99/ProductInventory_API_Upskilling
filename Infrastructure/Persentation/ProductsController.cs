b207dbcbe3ca2953086e1c065cd9dbb309c212d4
AbdElftah-Tarek <157211772+AbdElftah-Tarek@users.noreply.github.com>
2025-05-06 21:00:31 +0300

    test 3

--- Infrastructure/Persentation/ProductsController.cs
+++ Infrastructure/Persentation/ProductsController.cs
@@ -15,7 +15,7 @@ namespace Persentation
     [Route("api/[controller]")]
     public class ProductsController(IServiceManager serviceManager) : ControllerBase
     {
-        // Get Methods ss
+        // Get Methods ssss
         [HttpGet]
         public async Task<ActionResult<PaginatedResult<ProductResultDto>>> GetAllProducts([FromQuery] ProductSpecificationParameters specification)
             => Ok(await serviceManager.ProductService.GetAllProductsAsync(specification));
